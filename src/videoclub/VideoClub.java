62b9e1e76c2017931fb463b4ed0e3b4e642616da
saracho <emanuel>
2019-01-16 22:03:57 -0300

    Pantalla de la aplicacion

--- src/videoclub/VideoClub.java
+++ src/videoclub/VideoClub.java
@@ -1,13 +1,17 @@
 package videoclub;
 
 import View.ViewPantallaPrincipal;
+import View.ViewJFramePrincipal;
+import javax.swing.JFrame;
 import videoclub.VideoClub;
 
 public class VideoClub {
 
     public static void main(String[] args) {
 
-        ViewPantallaPrincipal.MnuPrincipal();
+        //ViewPantallaPrincipal.MnuPrincipal();
+        JFrame pantalla = new ViewJFramePrincipal();
+        pantalla.setVisible(true);
 
     }
 
