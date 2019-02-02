dbee80e1f45ec9d76e4acd3aa4a2c1b4d358f5f7
saracho <emanuel>
2019-02-01 21:57:52 -0300

    Implementacion del nuevo Ide

---
+++ src/View/PantallaPrincipal.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.*;
+
+public class PantallaPrincipal {
+    private JPanel panel1;
+    private JButton nuevoClienteButton;
+    private JButton buscarClienteButton;
+    private JButton editarClienteButton;
+    private JButton eliminarClienteButton;
+}
